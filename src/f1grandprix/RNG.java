3d60aeb4bf2918615ae7b2de6f5916d5d6002a7c
milan <milan@milan-Lenovo>
2021-03-26 00:49:31 +0100

    Added classes Driver and RNG

---
+++ src/f1grandprix/RNG.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package f1grandprix;
+
+import java.util.Random;
+
+/**
+ *
+ * @author milan
+ */
+public class RNG {
+    //atributi
+    private int minimumValue;
+    private int maximumValue;
+    private Random rnd;
+}
